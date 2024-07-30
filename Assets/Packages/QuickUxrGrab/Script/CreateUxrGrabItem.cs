2c79fe604d70af2d5e69a087d1db997f6f1a95c3
Jhundon De Leon Mendi <84818535+JDLMendi@users.noreply.github.com>
2024-07-30 10:10:42 +0100

    Revert "Pulling master into Ardri's branch"

--- Assets/Packages/QuickUxrGrab/Script/CreateUxrGrabItem.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-public class CreateUxrGrabItem : MonoBehaviour
-{
-    public GameObject tooltipPrefab; // Add this line to hold the Tooltip prefab
-}
-
