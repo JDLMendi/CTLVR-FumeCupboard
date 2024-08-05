190818b1b410a0404cae885bf77dec7bbfcceecc
Jhundon De Leon Mendi <84818535+JDLMendi@users.noreply.github.com>
2024-08-05 11:37:45 +0100

    Created Bulb Pipette

---
+++ Assets/BulbPipette.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UltimateXR.Manipulation;
+
+public class BulbPipette : MonoBehaviour
+{
+    // Determine which of the three grab point is being held.
+    [SerializeField] bool holdingAir;
+    [SerializeField] bool holdingEmpty;
+    [SerializeField] bool holdingSuction;
+
+}
